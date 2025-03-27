64cf2f74506bb9f0bb8b09e541666a7b3bfa474e
Guinueng <admin@guinueng.com>
2025-03-28 00:03:38 +0900

    Added some implementation detail on heap_sort.cc

--- heap_sort.cc
+++ heap_sort.cc
@@ -10,112 +10,49 @@ void swap(std::vector<int>& arr, size_t l_pos, size_t r_pos){
     arr[r_pos] = tmp;
 }
 
-// void downheap(std::vector<int>& arr, size_t idx){
-//     size_t l_pos = 2 * idx;
-//     size_t r_pos = 2 * idx + 1;
-
-//     if(l_pos > (arr.size() - 1) || r_pos > (arr.size() - 1)){
-//         return;
-//     }
-
-//     bool l = false;
-//     if(arr[l_pos] <= arr[r_pos]){
-//         l = true;
-//     }
-
-//     if(l){
-//         if(arr[idx] > arr[l_pos]){
-//             swap(arr, l_pos, idx);
-//             // int tmp = arr[l_pos];
-//             // arr[l_pos] = arr[idx];
-//             // arr[idx] = tmp;
-//         }
-//     }
-//     else{
-//         if(arr[idx] > arr[r_pos]){
-//             swap(arr, idx, r_pos);
-//             // int tmp = arr[r_pos];
-//             // arr[r_pos] = arr[idx];
-//             // arr[idx] = tmp;
-//         }
-//     }
-
-//     downheap(arr, l_pos);
-//     downheap(arr, r_pos);
-// }
-
 void max_heapify(std::vector<int>& arr, size_t idx, size_t len){
     size_t left = idx * 2;
     size_t right = idx * 2 + 1;
     size_t largest = idx;
 
-    // std::cout << "info: "  << left << " " << right << " "  << largest << " " << arr.size() << std::endl;
-
+    // 1. Check left child elem exist and it is bigger than root.
     if(left < len && arr.at(left) > arr.at(idx)){
         largest = left;
     }
-    else{
-        largest = idx;
-    }
+    // else{
+    //     largest = idx;
+    // }
 
+    // 2. Check right child elem exist and it is bigger than bigger one btw
+    //    left and target idx.
     if(right < len && arr.at(right) > arr.at(largest)){
         largest = right;
     }
 
-    // std::cout << "largest: " << largest <<std::endl;
-
+    // 3. If bigger than target idx, swap it and pursue max_heapify again to
+    //    make sure swapped one fits in correct position or need to go up again.
     if(largest != idx){
         swap(arr, idx, largest);
         max_heapify(arr, largest, len);
     }
-
-    // for(auto i : arr){
-    //     std::cout << i << " ";
-    // }
-    // std::cout << "\n";
 }
 
 void max_heap(std::vector<int>& arr, size_t idx){
     for(size_t i = (idx / 2); i > 0; i--){
+        // Since we just need to consider non-leaf nodes, idx is bound for
+        // (1, lower bound of (arr.size() / 2)).
         max_heapify(arr, i, arr.size());
     }
-
-    // size_t l_pos = 2 * idx;
-    // size_t r_pos = 2 * idx + 1;
-
-    // if(l_pos > (arr.size() - 1) || r_pos > (arr.size() - 1)){
-    //     return;
-    // }
-
-    // bool l = false;
-    // if(arr[l_pos] <= arr[r_pos]){
-    //     l = true;
-    // }
-
-    // if(l){
-    //     if(arr[idx] > arr[l_pos]){
-    //         swap(arr, l_pos, idx);
-    //         // int tmp = arr[l_pos];
-    //         // arr[l_pos] = arr[idx];
-    //         // arr[idx] = tmp;
-    //     }
-    // }
-    // else{
-    //     if(arr[idx] > arr[r_pos]){
-    //         swap(arr, idx, r_pos);
-    //         // int tmp = arr[r_pos];
-    //         // arr[r_pos] = arr[idx];
-    //         // arr[idx] = tmp;
-    //     }
-    // }
-
-    // downheap(arr, l_pos);
-    // downheap(arr, r_pos);
 }
 
 void heap_sort(std::vector<int>& arr){
-    // downheap(arr, 1);
+    // 1. Make max_heap tree.
     max_heap(arr, arr.size() - 1);
+
+    // 2. Since root is sorted as maximum elem, swap it into last elem,
+    //    then, operate max_heapify again w/o swapped elem.
+    //    Then, 2nd max elem will come to root, and keep going to make
+    //    element sorted.
     for(size_t i = (arr.size() - 1); i > 0 ; i--){
         swap(arr, 1, i);
         max_heapify(arr, 1, i);
@@ -123,13 +60,6 @@ void heap_sort(std::vector<int>& arr){
 
 }
 
-// int pop_rm_front(std::vector<int>& arr){
-//     int pop_num = arr.at(0);
-//     arr.at(1) = arr.at((arr.size() - 1));
-//     arr.pop_back();
-
-//     return pop_num;
-// }
 
 int main(int argc, char* argv[]){
     // If input on argument is not proper, send it to error handler.

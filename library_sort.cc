fa9df38e1611ab031265dd1261ca8c5c01de4e4d
Guinueng <admin@guinueng.com>
2025-03-30 15:50:44 +0900

    Implemented library_sort.cc

--- library_sort.cc
+++ library_sort.cc
@@ -5,6 +5,7 @@
 #include <climits>
 #include <chrono>
 #include <cmath>
+#include <bits/stdc++.h>
 
 void swap(std::vector<int>& arr, size_t l_pos, size_t r_pos){
     int tmp = arr[l_pos];
@@ -13,11 +14,12 @@ void swap(std::vector<int>& arr, size_t l_pos, size_t r_pos){
 }
 
 size_t bin_search(int target, const std::vector<int>& sorted, size_t high) {
+    // std::cout << "bin\n";
     size_t low = 0;
     while (low < high){
         size_t mid = low + (high - low) / 2;
         size_t pos = mid;
-
+        // std::cout << "high: " << high << " mid: " << mid << " low: " << low << " pos: " << pos <<"\n";
         while (pos < sorted.size() && sorted[pos] == INT_MIN){
             pos++;
         }
@@ -32,30 +34,124 @@ size_t bin_search(int target, const std::vector<int>& sorted, size_t high) {
 }
 
 void rebalance(std::vector<int>& arr, size_t f, size_t b){
-    arr.resize(2 * arr.size());
-
+    size_t before_size = arr.size();
+    // std::cout << "bef: " << before_size << "\n";
+    arr.resize(2 * before_size);
+    for(size_t i = before_size; i < 2 * before_size; i++){
+        arr[i] = INT_MIN;
+    }
+    // std::cout << "bef prog" << "\n";
+    b--;
     while(b >= f){
-        arr[2 * b] = INT_MIN;
+        //arr[2 * b] = INT_MIN;
+        // std::cout << "b: " << b << "\n";
         swap(arr, b, 2 * b);
+        if(b == 0)
+            break;
         b--;
     }
 }
 
 void library_sort(std::vector<int>& arr){
-    size_t n = arr.size() - 1;
-    std::vector<int> sorted(2 * (n), INT_MIN);
+    size_t n = arr.size();
+    std::vector<int> sorted(2, INT_MIN);
 
-    size_t size = 2;
-    for(size_t i = 1; int(ceil(log2(n - 1))); i++){
-        rebalance(sorted, 1, size / 2);
-        for(size_t j = size / 2; j <= size; j++){
-            size_t pos = bin_search(arr[j], sorted, size);
-            sorted[pos] = arr[j];
-        }
-        size *= 2;
+    // std::cout << "n: " << n << " log n: " << int(ceil(log2(n - 1))) << " \n";
+    size_t size = 0;
+    for(size_t i = 0; i < n; i++){
+        // for(auto i : sorted){
+            // std::cout << i << " ";
+        // }
+        // std::cout << "i: " << i << " size: " << sorted.size() << "\n";
+        //if(size >= sorted.size()){
+        //    std::cout << "rebalance need\n";
+        //    rebalance(sorted, 0, sorted.size());
+        //    
+        //    for(auto i : sorted){
+        //        std::cout << i << " ";
+        //    }
+        //    std::cout << "rebalance complete\n";
+        //}
+        //for(size_t j = size / 2; j <= size; j++){
+            
+            if(size >= sorted.size()){
+                // std::cout << "rebalance need\n";
+                rebalance(sorted, 0, sorted.size());
+                
+                // for(auto i : sorted){
+                    // std::cout << i << " ";
+                // }
+                // std::cout << "rebalance complete\n";
+                //std::cout << "target: " << arr[size] << " pos: " << size << "\n";
+                //pos = bin_search(arr[size], sorted, sorted.size());
+                //std::cout << "pos: " << pos << "\n";
+            }
+
+            // std::cout << "target: " << arr[size] << " pos: " << size << "\n";
+            size_t pos = bin_search(arr[size], sorted, sorted.size());
+            // std::cout << "pos: " << pos << "\n";
+
+            if(pos >= sorted.size()){
+                pos = sorted.size() - 1;
+            }
+            // std::cout << "2 pos: " << pos << "\n";
+            if(sorted[pos] != INT_MIN){
+                bool solved = false;
+                while(!solved){
+                    bool low = false, high = false;
+                    size_t mov_end = pos;
+                    if(sorted[pos] >= arr[size] || high){
+                        high = false;
+                        // std::cout << 1 << "\n";
+                        while(sorted[mov_end] != INT_MIN){
+                            mov_end++;
+                            if(mov_end >= sorted.size()){
+                                low = true;
+                                break;
+                            }
+                        }
+                        // std::cout << "mov_end: " << mov_end << "\n";
+                        //if(mov_end > sorted.size()){
+                        //    low = true;
+                        //}
+                        if(!low){
+                            for(size_t i = mov_end; i > pos; i--){
+                                sorted[i] = sorted[i - 1];
+                            }
+                            solved = true;
+                        }
+                    }
+                    if(sorted[pos] < arr[size] || low){
+                        if(low){
+                            low = false;
+                            pos--;
+                        }
+                        // std::cout << 2 << "\n";
+                        size_t mov_front = pos;
+                        // std::cout << mov_front << "\n";
+                        while(sorted[mov_front] != INT_MIN){
+                            if(mov_front == 0){
+                                high = true;
+                                break;
+                            }
+                            mov_front--;
+                        }
+                        // std::cout << "mov_fornt: " << mov_front << "\n";
+                        if(!high){
+                            for(size_t i = mov_front; i < pos; i++){
+                                sorted[i] = sorted[i + 1];
+                            }
+                            solved = true;
+                        }
+                    }
+                }
+            }
+            sorted[pos] = arr[size];
+        //}
+        size++;
     }
 
-    size_t idx = 1;
+    size_t idx = 0;
     for(auto i : sorted){
         if(i != INT_MIN){
             arr[idx++] = i;
@@ -86,9 +182,6 @@ int main(int argc, char* argv[]){
     // Allocate vector to store numbers read in file.
     std::vector<int> numbers;
     numbers.reserve(1000001);
-    
-    // Add useless value to make padding.
-    numbers.push_back(0);
 
     // Read numbers from file.
     int number;
@@ -120,9 +213,6 @@ int main(int argc, char* argv[]){
     // End measuring sort_func finish time
     auto sort_end = std::chrono::high_resolution_clock::now();
 
-    // Delete first element in vector utilized for padding.
-    numbers.erase(numbers.begin());
-
     // Save result into new file.
     std::ofstream outFile(outputFile);
     if (!outFile) {
@@ -145,6 +235,13 @@ int main(int argc, char* argv[]){
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     auto sort_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(sort_end - sort_start).count();
     std::cout << "Elapsed time: " << elapsed << " ms\n" << "Sorting time: " << sort_elapsed << " ms\n" << std::endl;
+
+    if(std::is_sorted(numbers.begin(), numbers.end())){
+        std::cout << "Sorted!\n";
+    }
+    else{
+        std::cout << "ERROR!\n";
+    }
 
     return 0;
 }
